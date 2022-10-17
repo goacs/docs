04857362110511465142bcc5fcdf7fd61b0707df
pingwiniasty <pingwiniasty@users.noreply.github.com>
2022-10-17 21:32:46 +0000

    deploy: 3ecf15dc1e706f067a415b99d97520ed7043a0bd

---
+++ assets/guide_scripting.md.202f1d7a.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,a as p}from"./app.52ff9ae1.js";const y=JSON.parse('{"title":"Scripting","description":"","frontmatter":{},"headers":[],"relativePath":"guide/scripting.md","lastUpdated":1666042333000}'),e={name:"guide/scripting.md"},o=p("",3),t=[o];function l(c,r,i,D,d,_){return n(),a("div",null,t)}const C=s(e,[["render",l]]);export{y as __pageData,C as default};
